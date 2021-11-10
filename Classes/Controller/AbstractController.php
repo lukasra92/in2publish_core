b030213e912fbb578058a09d00ce30efac74e486
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2021-11-10 15:17:13 +0100

    [UPGRADE] Remove call of deprecated method ExtbaseActionController::initializeView
    see: https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.5/Deprecation-95222-ExtbaseViewInterface.html

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -105,6 +105,7 @@ abstract class AbstractController extends ActionController
         }
     }
 
+    // TODO v12: replace TYPO3\CMS\Extbase\Mvc\View\ViewInterface with TYPO3Fluid\Fluid\View\ViewInterface
     protected function initializeView(ViewInterface $view): void
     {
         parent::initializeView($view);
